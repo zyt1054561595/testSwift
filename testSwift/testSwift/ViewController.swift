e8c5e8bee2465eec11e6a1dd7222dbd5725b755e
zyt1054561595 <zhangyongtao_cool@126.com>
2019-10-09 22:30:18 +0800

    添加注销的，查看diff有什么不同

--- testSwift/testSwift/ViewController.swift
+++ testSwift/testSwift/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.view.backgroundColor = UIColor.red;
+//        UITableView * tab = [UITableView ];
     }
 
 
